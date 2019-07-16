d4e196a220db2494c4359cb677bbc81a31e57d70
Zion Brown <zabs95@gmail.com>
2019-07-16 01:30:14 -0400

    I added a javascript file, and a bunch of CSS.

---
+++ assets/js/logic.js
@@ -0,0 +1,11 @@
+function toAbout() {
+    window.location.href = 'index.html';
+}
+
+function toPortfolio() {
+    window.location.href = 'portfolio.html';
+}
+
+function toContact() {
+    window.location.href = 'contact.html';
+}
